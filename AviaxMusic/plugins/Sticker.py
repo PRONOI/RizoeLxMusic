b0f66ddbd307d4130cfc147722dce10b3a21098f
PRANAY <84889578+PRONOI@users.noreply.github.com>
2024-11-17 02:29:40 +0530

    Update Sticker.py

--- AviaxMusic/plugins/Sticker.py
+++ AviaxMusic/plugins/Sticker.py
@@ -4,5 +4,5 @@ from AviaxMusic import app
 @app.on_message(filters.command("start"))
 async def start(client, message):
     # Replace 'sticker_file_id' with the actual file ID or URL of the sticker
-    sticker_file_id = "CAACAgUAAxkBAAIGJmc4uIA18pKbZrwXou93tqBwDOL-AAJaEwACIK7BVdo1lpGVyOvgNgQ"
+    sticker_file_id = "CAACAgUAAxkBAAJE8GK4EsoLVZC2SW5W5Q-QAkaoN8f_AAL9BQACiy14VGoQxOCDfE1KKQQ"
     await message.reply_sticker(sticker_file_id)
