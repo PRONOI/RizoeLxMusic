cce6928117485a6f027a959e7dcf8c04fcf3e635
PRANAY <84889578+PRONOI@users.noreply.github.com>
2024-11-17 11:49:31 +0530

    Update start.py

--- AviaxMusic/plugins/bot/start.py
+++ AviaxMusic/plugins/bot/start.py
@@ -25,12 +25,10 @@ from config import BANNED_USERS
 from strings import get_string
 
 
-
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
-    await message.react("❤️")
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
